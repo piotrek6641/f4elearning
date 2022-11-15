480222571c90897c56df12b25319dcadd9a7b7e6
Piotr <piotrek.falba@gmail.com>
2022-11-15 11:57:41 +0000

    added fillable properties

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,4 +11,8 @@ class Category extends Model
     public function lessons(){
         return $this->hasMany(Lesson::class);
     }
+    protected $fillable=
+        [
+          'title'
+        ];
 }
