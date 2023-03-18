18105192fd25d2fcc53864517918f200c6add57f
Piotr <piotrek.falba@gmail.com>
2023-03-18 18:55:30 +0000

    Controllers to menage users data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,4 +21,63 @@ class UserController extends Controller
         }
         return to_route('settings');
     }
+    public function Search(Request $request)
+    {
+        if($request->option =="id")
+        {
+            $users = User::where('id', $request->text)->paginate(10);
+            return view('users.index')->with('users',$users);
+        }
+        if($request->option == "name") {
+            $users = User::where('name', 'LIKE', $request->text . '%')->paginate(10);
+            return view('users.index')->with('users',$users);
+        }
+        if($request->option=="email")
+        {
+            $users = User::where('email', 'LIKE', $request->email . '%')->paginate(10);
+            return view('users.index')->with('users',$users);
+        }
+        else
+        {
+            return back();
+        }
+
+    }
+    public function Filter(Request $request)
+    {
+        if($request->option =="id")
+        {
+            $users = User::orderBy('id')->paginate(10);
+            return view('users.index')->with('users',$users);
+        }
+        if($request->option == "name") {
+            $users = User::orderBy('name')->paginate(10);
+            return view('users.index')->with('users',$users);
+        }
+        if($request->option=="email")
+        {
+            $users = User::orderBy('email')->paginate(10);
+            return view('users.index')->with('users',$users);
+        }
+        if($request->option=="updated_at")
+        {
+            $users = User::orderBy('updated_at', 'desc')->paginate(10);
+            return view('users.index')->with('users',$users);
+        }
+        else
+        {
+            return back();
+        }
+
+    }
+    public function ban($id)
+    {
+        $user = User::where('id', $id)->first();
+        if ($user->isbaned == 0) {
+            $user->update([
+                'isbaned' => 1
+            ]);
+            return back();
+        }
+    }
 }
---
+++ app/Http/Controllers/Users1Controller.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+
+use Illuminate\Validation\Rules;
+
+class Users1Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::All();
+        $users = User::paginate(10);
+        return view('users/index')->with('users', $users);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return  view('add-user');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required' ],
+        ]);
+
+        $user=User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'isbaned' => 0
+        ]);
+        event(new Registered($user));
+        return to_route('dashboard');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::where('id',$id)->first();
+        return view('edit-user')->with('user',$user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::where('id',$id)->first();
+        return view('users/edit-user')->with('user',$user);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        $request->validate([
+            'name' => 'required|max:120',
+            'email' => 'required'
+        ]);
+        $user = User::where('id',$id)->first();
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => $request->role
+        ]);
+    return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::where('id',$id)->delete();
+        return to_route('dashboard');
+    }
+}
